2d3d59f4a8826414d910bb43c32d578fcce7fc13
Aaryan Pugarja <85410288+Aaryan-Pugarja@users.noreply.github.com>
2026-03-19 10:37:24 +0530

    Update courses.pl

--- Prolog/19 Feb/courses.pl
+++ Prolog/19 Feb/courses.pl
@@ -1,5 +1,5 @@
+% facts
 easy(hardware).
-
 easy(graphics).
 
 not_easy(logic).
@@ -7,32 +7,16 @@ not_easy(logic).
 books_available(hardware).
 books_available(database).
 
-credits(graphics, 8).
+eight_credits(graphics).
 lab(graphics).
 
 takes(mary, compiler).
 
-takes(X, Y) :-
+% rules
+takes(_, Y) :-
     easy(Y),
     books_available(Y).
 
-takes(X, Y) :-
-    credits(Y, 8),
+takes(_, Y) :-
+    eight_credits(Y),
     lab(Y).
-
-main :-
-    % 1) Does Mary take graphics?
-    (takes(mary, graphics) ->
-        writeln('Yes, Mary takes graphics.');
-        writeln('No, Mary does not take graphics.')
-    ),
-
-    % 2) Which course does Mary take?
-    writeln('Courses Mary takes:'),
-    forall(takes(mary, C),
-           writeln(C)),
-
-    % 3) Who takes graphics?
-    writeln('Students who take graphics:'),
-    forall(takes(S, graphics),
-           writeln(S)).
